3c19347316bac3efb6c91e8a9ffce9af9fc879c8
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-09 15:11:48 -0300

    Alterando título, descrição e tags dos posts

--- data/posts.php
+++ data/posts.php
@@ -3,30 +3,30 @@
   $posts = [
     [
       'id' => 1,
-      'title' => 'Programando em PHP',
-      'description' => 'Neste post você vai aprender tudo que precisa para ser um bom programador de PHP!',
-      'tags' => ['programação', 'PHP', 'lógica de programação'],
+      'title' => 'Primeira Imagem',
+      'description' => 'Descrição do 1º post',
+      'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-1.jpg'
     ],
     [
       'id' => 2,
-      'title' => 'Orientação a Objetos com Java',
-      'description' => 'Aprenda o paradigma de orientação a objetos com Java, uma linguagem em alta no mercado.',
-      'tags' => ['Java', 'OOP', 'Programação Avançada'],
+      'title' => 'Segunda Imagem',
+      'description' => 'Descrição do 2º post',
+      'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-2.jpg'
     ],
     [
       'id' => 3,
-      'title' => 'A importância da lógica de programação',
-      'description' => 'A lógica de programação é o ponto inicial de muitos estudantes de programação, será que é importante?',
-      'tags' => ['lógica', 'programação', 'algoritmos'],
+      'title' => 'Terceira Imagem',
+      'description' => 'Descrição do 3º post',
+      'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-3.jpg',
     ],
     [
       'id' => 4,
-      'title' => 'Programando em PHP',
-      'description' => 'Neste post você vai aprender tudo que precisa para ser um bom programador de PHP!',
-      'tags' => ['programação', 'PHP', 'lógica de programação'],
+      'title' => 'Quarta Imagem',
+      'description' => 'Descrição do 4º post',
+      'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-4.jpg'
     ],
   ];
