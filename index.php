cc9268f4a79dbb995ef8fc62243966c3be0db53d
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-09 14:35:29 -0300

    Adição de imagens no arquivo index

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
     include_once("templates/header.php")
 ?>
-
-    <h1>Hello World</h1>
-
+    <main>
+        <div id="title-container">
+            <h1> Nome do Blog</h1>
+            <p>Slogan do Blog</p>
+        </div>
+        <div id="posts-container">
+            <?php foreach ($posts as $post): ?>
+                <div class="post-box">
+                    <img src="<?= $BASE_URL ?>/images/<?= $post['image']?>" alt="">
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </main>
 <?php
     include_once("templates/footer.php")
 ?>
