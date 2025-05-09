c0dc3c93d58f80f40e8c8d195901b2c8a818c390
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-09 01:41:37 -0300

    Adição de fontes

--- index.php
+++ index.php
@@ -10,9 +10,14 @@ include_once("helpers/url.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
+    <!-- Estilos do Projeto -->
     <link rel="stylesheet" href="<?=$BASE_URL ?>/css/styles.css">
+    <!-- Google Fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+
 </body>
 </html>
