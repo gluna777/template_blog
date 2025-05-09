0888f91e3bad69ec9c446cc194728e85775785fc
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-09 01:20:01 -0300

    Criação do repositório css

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include_once("helpers/url.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?=$BASE_URL ?>/css/styles.css">
 </head>
 <body>
     
