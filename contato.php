f35567218a1f667678f8c86af98439b64d282f17
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-12 13:05:21 -0300

    Adicionando informações ao arquivo contato

--- contato.php
+++ contato.php
@@ -2,6 +2,10 @@
     include_once("templates/header.php")
 ?>
     <h1>Página de Contato</h1>
+    <h4>Dados de contato</h4>
+    <a href="https://www.gmail.com/">Email: suaconta@email.com</a>
+    <p>Telefone: +55 (00) 00000-0000</p>
+    <a href="https://www.linkedin.com/">Linkendin: www.linkedin.com/in/suaconta</a>
 <?php
     include_once("templates/footer.php")
 ?>
