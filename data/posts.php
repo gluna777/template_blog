56e1e8fd704dd031a799c0c329247c8b3329493d
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-12 13:12:12 -0300

    Alterando título no arquivo posts

--- data/posts.php
+++ data/posts.php
@@ -3,28 +3,28 @@
   $posts = [
     [
       'id' => 1,
-      'title' => 'Primeira Imagem',
+      'title' => 'Primeiro Titulo',
       'description' => 'Descrição do 1º post',
       'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-1.jpg'
     ],
     [
       'id' => 2,
-      'title' => 'Segunda Imagem',
+      'title' => 'Segundo Titulo',
       'description' => 'Descrição do 2º post',
       'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-2.jpg'
     ],
     [
       'id' => 3,
-      'title' => 'Terceira Imagem',
+      'title' => 'Terceiro Titulo',
       'description' => 'Descrição do 3º post',
       'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-3.jpg',
     ],
     [
       'id' => 4,
-      'title' => 'Quarta Imagem',
+      'title' => 'Quarto Titulo',
       'description' => 'Descrição do 4º post',
       'tags' => ['1ª tag relacionada ao post', '2ª tag', '3ª tag' ],
       'image' => 'prog-4.jpg'
