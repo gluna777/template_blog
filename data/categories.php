95d64fd7ed7ce312cf861106f2b2977826751e9a
Gustavo Luna Procopio <gugalunaprocopio@gmail.com>
2025-05-12 13:14:11 -0300

    Alterção das categorias no arquivo categories

--- data/categories.php
+++ data/categories.php
@@ -1,11 +1,13 @@
 <?php
 
+/* categorias relacionadas aos posts */
+
   $categories = [
-    'PHP',
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'Bootstrap',
-    'Java',
-    'C#'
+    '1ª categoria',
+    '2ª categoria',
+    '3ª categoria',
+    '4ª categoria',
+    '5ª categoria',
+    '6ª categoria',
+    '7ª categoria'
   ];
